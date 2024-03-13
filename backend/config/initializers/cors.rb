74a51b5337b756a4d8aac63a689fec9bd334ec04
Bao Ngo <ngobao2001@gmail.com>
2024-03-13 20:14:22 +0700

    Fix path

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3002" # React apps
+    origins "localhost:3000" # React apps
 
     resource "*",
       headers: :any,
